e3f5c42c9c9e928369d7bb9a87c2d69f53b9b4ae
Jonathan Kristanto <kristantojonathan@gmail.com>
2021-11-04 14:49:18 +0700

    feat: landing Page & login (view only)

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        if($request->name){
+
+        }
+
+        if($request->age){
+
+        }
+
+        if($request->password){
+
+        }
+
+        return redirect()->home();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', function () {
+    return view('welcome');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('register', function () {
+    return view('');
+});
