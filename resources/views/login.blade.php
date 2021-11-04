b955bafb0138c22672c81822ba7ac56a5127151b
Jonathan Kristanto <kristantojonathan@gmail.com>
2021-11-04 16:55:26 +0700

    feat: Add style to login blade

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,48 +1,75 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Signin Template · Bootstrap v5.1</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sign-in/">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <style>
-        .view-table thead th, .view-table thead td, .view-table thead tr{
-            border: 1px solid black;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
 
-        .row {
-            margin-bottom: 20px;
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
         }
     </style>
+
+    <!-- Custom styles for this template -->
+    <link href="/css/style.css" rel="stylesheet">
+
 </head>
-<body>
-    <h1>LOGIN</h1>
-    {{-- Insert Brand --}}
-    <div class="row">
+
+<body class="text-center">
+
+    <main class="form-signin">
+        <h1 class="h3 mb-3 fw-normal">Login Page</h1>
         <form action="/login" method="POST">
             @csrf
-            <table>
-                <tr>
-                    <td><label for="email">Email</label></td>
-                    <td><input type="email" id="email" name="email" placeholder="Sensei@email.com"></td>
-                </tr>
-                <tr>
-                    <td><label for="name">Password</label></td>
-                    <td><input type="password" id="password" name="password" placeholder="password"></td></tr>
-                </tr>
-            </table>
-            <button type="submit">Login</button>
-        </form>
-    </div>
-    <a href="/home"><button>Go Back</button></a>
+            <div class="form-floating">
+                <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
 
-    @if($errors->any())
+            <div class="form-floating">
+                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+        </form>
+
+        <button class="w-100 mt-1 btn btn-lg btn-secondary" onclick="location.href = 'home';">Go Back</button>
+        <small class="d-block text-center mt-3"> Not Registered? <a href="register"> Register Now!</a></small>
+        <p class="mt-5 mb-3 text-muted">&copy; November 2021</p>
+
+        @if($errors->any())
         <table>
             <tr>Error while login:</tr>
             @foreach ($errors->all() as $error)
-                <tr><li style="color:red">{{ $error }}</li></tr>
+            <tr>
+                <li style="color:red">{{ $error }}</li>
+            </tr>
             @endforeach
         </table>
-    @endif
+        @endif
+    </main>
 </body>
+
 </html>
